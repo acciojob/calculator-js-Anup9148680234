2b0e963d8255ffcb9b3a6c0406cab4eb87b1a4c4
Anup Angadi <52240005+Anup9148680234@users.noreply.github.com>
2023-02-24 23:11:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 //your JS code here. If required.
+let input = document.getElementById("input");
+let expression = "";
+function solve(e) {
+  // console.log(e)
+  if (e === "ans") {
+    // evaluate the expression
+    let result = eval(expression);
+    console.log(result);
+    input.value = result;
+  } else if (e === "clear") {
+    expression = "";
+    input.value = "";
+  } else {
+    expression += e;
+    console.log(expression);
+    input.value = expression;
+  }
+}
+
+function backspace() {
+    var inputValue = input.value;
+
+    var newValue = inputValue.slice(0, -1);
+
+    input.value = newValue;
+  }
