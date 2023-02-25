190e9b344b6e20f079008a19d70d2126b15fd566
Anup Angadi <52240005+Anup9148680234@users.noreply.github.com>
2023-02-25 22:21:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,31 +1,3 @@
-//your JS code here. If required.
-
-//your code here
-// const arr1 = [1,2,3,4,5]
-
-
-// function makeItDoublexyz(xyz){
-//     return "rohan"+"1"
-// }
-// const arr5 = arr1.map(x=>makeItDoublexyz(x))
-// // const arr2 = arr1.map(makeItDoublexyz)
-
-// // Both are same
-
-
-// console.log(arr1)
-// console.log(arr5)
-/*
-7
-78
-78.
-exp = 78.4/35 
-= => eval(exp)
-
-C => display should get empty
-<- => just remove last character
-*/
-
 let displayxyz = document.getElementById("display")
 
 let buttonsxyz = Array.from(document.getElementsByClassName("btn"))
